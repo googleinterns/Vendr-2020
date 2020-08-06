a6b381b94ed12c36b12c1971dbde2a3cf3e64e9b
Angel Guevara <angelguevarahdz.io@gmail.com>
2020-08-06 16:28:31 +0000

    Add UpdateVendor Servlet

---
+++ src/main/java/com/google/sps/servlets/UpdateVendorServlet.java
@@ -0,0 +1,158 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.EmbeddedEntity;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.GeoPt;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.sps.COMMONS;
+import com.google.sps.data.HttpServletUtils;
+import com.google.sps.data.Vendor;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * When the user submits the form, Blobstore processes the file upload and then forwards the request
+ * to this servlet to update a vendor information
+ */
+@WebServlet("/update-vendor")
+public class UpdateVendorServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    
+    if (userService.isUserLoggedIn()) {
+      String firstName = HttpServletUtils.getParameter(request, "firstName", "");
+      String lastName = HttpServletUtils.getParameter(request, "lastName", "");
+      String phoneNumber = HttpServletUtils.getParameter(request, "phoneNumber", "");
+      
+      // Get values to add/update vendor's profile picture
+      String currentBlobKey = HttpServletUtils.getParameter(request, "blobKey", "");
+      BlobKey imageBlobKey = getUploadedFileBlobKey(request, "imageFile");
+      String altText = HttpServletUtils.getParameter(request, "altText", "");
+      // If nothing was uploaded and there is a current picture, keep current
+      if (!currentBlobKey.isEmpty() && imageBlobKey == null) {
+        imageBlobKey = new BlobKey(currentBlobKey);
+      }
+
+      // Check values are not empty or outside range
+      if (firstName.isEmpty() || lastName.isEmpty() || phoneNumber.isEmpty() || altText.isEmpty() || 
+          imageBlobKey == null) {
+        System.out.println("The values do not exist and/or are outside the range.");
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+      }
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+      Key vendorKey = KeyFactory.createKey("Vendor", userService.getCurrentUser().getUserId());
+      Entity vendorEntity;
+      try {
+        vendorEntity = datastore.get(vendorKey);
+      } catch (EntityNotFoundException e) {
+        System.out.println(e);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+      }
+      Vendor vendorObject = new Vendor(vendorEntity);
+
+      Entity picture;
+      // If the vendor doesn't have a picture, create completely new entity
+      // Else, use the already existing entity
+      if (vendorObject.getProfilePic() == null) {
+        picture = new Entity("Picture", vendorKey);
+      } else {
+        picture = new Entity("Picture",
+            vendorObject.getProfilePic().getId(), vendorKey);
+      }
+
+      // If not the same, delete previous blob from blobstore
+      if (vendorObject.getProfilePic() != null && 
+          imageBlobKey.compareTo(vendorObject.getProfilePic().getBlobKey()) != 0) {
+        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+        blobstoreService.delete(vendorObject.getProfilePic().getBlobKey());
+      }
+      picture.setProperty("blobKey", imageBlobKey);
+      picture.setProperty("altText", altText);
+      datastore.put(picture);
+
+      EmbeddedEntity picInfo = new EmbeddedEntity();
+      picInfo.setKey(picture.getKey());
+      picInfo.setPropertiesFrom(picture);
+
+      vendorEntity.setProperty("firstName", firstName);
+      vendorEntity.setProperty("lastName", lastName);
+      vendorEntity.setProperty("phoneNumber", phoneNumber);
+      vendorEntity.setProperty("profilePic", picInfo);
+      datastore.put(vendorEntity);
+      
+      response.sendRedirect("/");
+    } else {
+      System.out.println("User is not logged in.");
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+  }
+
+  /** Returns a BlobKey for the uploaded file, or null if the user didn't upload a file. */
+  private BlobKey getUploadedFileBlobKey(HttpServletRequest request, String formInput) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get(formInput);
+
+    // User submitted form without selecting a file (dev server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo == null || blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // Validate the file is an image file (.png, .jpg, .jpeg, .jfif, .pjpeg, .pjp)
+    String blobMimeType = blobInfo.getContentType();
+    if (blobMimeType.equals("image/png") || blobMimeType.equals("image/jpeg")) {
+      return blobKey;
+    } else {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+  }
+}
