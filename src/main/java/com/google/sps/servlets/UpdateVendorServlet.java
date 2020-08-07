348b93aba5788a79142465050e45d985bb593013
Angel Guevara <angelguevarahdz.io@gmail.com>
2020-08-07 19:20:23 +0000

    Remove import

--- src/main/java/com/google/sps/servlets/UpdateVendorServlet.java
+++ src/main/java/com/google/sps/servlets/UpdateVendorServlet.java
@@ -21,7 +21,6 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.EmbeddedEntity;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.UserService;
