0f1ce2eaa14a9babc6727444358af2007688ca3c
RicardoChapaRomero <ricardochaparomero@gmail.com>
2020-08-18 13:09:19 +0000

    Prepare branch for rebase

--- src/main/webapp/scripts/nearbyVendors.js
+++ src/main/webapp/scripts/nearbyVendors.js
@@ -113,9 +113,3 @@ const addVendorsToMap = (map, nearbyVendors) => {
   });
 };
 
-/**
-* To insert the search bar when page loads and then initialize the map of the webpage.
-*/
-$(() => {
-  $('#searchBarVendors-placeholder').load('common/searchBarVendors.html', initMap);
-});
